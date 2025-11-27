7171d3e8266ddd33448a8c6aa4e3fbcc36d6eb4d
KoliaK <xkoliakx@gmail.com>
2025-11-27 16:29:35 -0300

    Day 2 implemented modularized commands and included logging

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import os
+import logging
 from telegram.ext import ApplicationBuilder, CommandHandler
 from dotenv import load_dotenv
 
-# Gets credentials from .env file
+#scripts
+from commands import start, help
+
+# gets credentials from .env file
 load_dotenv()
 bot_api_token = os.getenv('TELEGRAM_TOKEN')
-bot_name = os.getenv('BOT_NAME')
+# bot_name = os.getenv('BOT_NAME')
 
-application = ApplicationBuilder().token(bot_api_token).build()
-
-
-
-
-async def start(update, context):
-    await update.message.reply_text(f'{bot_name} is online. awaiting instructions.')
-
-
+# prints logs to the terminal
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
 if __name__ == '__main__':
+    # build the app
+    application = ApplicationBuilder().token(bot_api_token).build()
+
+    # add command handlers
     application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('help', help))
+    
+    print('Bot is polling...')
     application.run_polling() #for large-scale projects, use .run_webhook() instead
